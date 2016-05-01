2119789885136d1a9ad6f2e59cb3362efd7f9302
Jonathan Queiroz <dev@johnjq.com>
2016-05-01 13:37:13 -0300

    Add support for docker.

---
+++ docker-stop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+########################## Stop containers ##########################
+echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Stopping containers..."
+previously_running=false
+
+if [ -n "`docker $DOCKER_OPTS ps -a | grep lojto-server$`" ]; then
+	previously_running=true
+	echo -ne "\t"
+	docker $DOCKER_OPTS stop lojto-server
+fi
+
+echo ""
+if [ "$previously_running" = false ]; then
+	exit
+fi
+
+########################## Remove containers ##########################
+echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Removing containers..."
+
+if [ -n "`docker $DOCKER_OPTS ps -a | grep lojto-server$`" ]; then
+	echo -ne "\t"
+	docker $DOCKER_OPTS rm lojto-server
+fi
+
+echo ""
