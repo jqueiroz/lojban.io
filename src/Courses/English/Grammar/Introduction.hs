b3e7c1a3f48e3be7f3ad83595cd3ce5a53c4f743
Jonathan Queiroz <dev@johnjq.com>
2016-05-07 16:26:05 -0300

    Add alternative English translations to some sentences.

--- src/Courses/English/Grammar/Introduction.hs
+++ src/Courses/English/Grammar/Introduction.hs
@@ -72,9 +72,9 @@ vocabulary3 = createVocabularyBuilder
 -------- Translations
 translations1_nice :: [Translation]
 translations1_nice =
-    [ ("do dunda ti mi", ["You gave me this."])
-    , ("mi dunda ta do", ["I gave you that."])
-    , ("mi tavla mi", ["I am talking to myself."])
+    [ ("do dunda ti mi", ["You gave me this.", "You gave us this."])
+    , ("mi dunda ta do", ["I gave you that.", "We gave you that."])
+    , ("mi tavla mi", ["I am talking to myself.", "I was talking to myself", "We were talking to ourselves."])
     ]
 
 translations1 :: [Translation]
@@ -82,40 +82,41 @@ translations1 = translations1_nice ++
     [ ("zdani mi", ["I have a house."])
     , ("do pendo mi", ["You are my friend."])
     , ("mi pendo do", ["I am your friend."])
-    , ("mi tavla do", ["I am talking to you."])
-    , ("do tavla mi", ["You are talking to me."])
+    , ("mi tavla do", ["I am talking to you.", "I was talking to you.", "We are talking to you.", "We were talking to you."])
+    , ("do tavla mi", ["You are talking to me.", "You are talking to us."])
     , ("do tavla do", ["You are talking to yourself."])
-    , ("mi prenu", ["I am a person."])
-    , ("do prenu", ["You are a person."])
-    , ("ta zdani", ["That is a house."])
-    , ("ta mlatu", ["That is a cat."])
+    , ("mi prenu", ["I am a person.", "We are persons."])
+    , ("do prenu", ["You are a person.", "You are persons."])
+    , ("ta zdani", ["That is a house.", "Those are houses."])
+    , ("ti mlatu", ["This is a cat.", "These are cats."])
+    , ("ta mlatu", ["That is a cat.", "Those are cats."])
     ]
 
 translations2_nice :: [Translation]
 translations2_nice =
-    [ ("mi nelci lo dunda ku", ["I like the donor."])
-    , ("lo se dunda ku melbi", ["The gift is beautiful."])
-    , ("mi te dunda lo gerku ku", ["I was given a dog."])
+    [ ("mi nelci lo dunda ku", ["I like the donor.", "I like the donors."])
+    , ("lo se dunda ku melbi", ["The gift is beautiful.", "The gifts are beautiful"])
+    , ("mi te dunda lo gerku ku", ["I was given a dog.", "We were given a dog.", "We were given dogs.", "Somebody gave me a dog.", "Somebody gave us a dog.", "Somebody gave us dogs."])
     , ("lo ctuca ku se zdani", ["The instructor has a house."])
     , ("mi nelci lo xe ctuca ku", ["I like the teaching method."])
-    , ("lo se tavla ku prenu", ["The listener is a person."]) -- is "listener" a good choice?
-    , ("lo tavla ku pendo mi", ["The speaker is my friend."]) -- is "speaker" a good choice? maybe it implies voice...
+    , ("lo se tavla ku prenu", ["The listener is a person.", "The listeners are persons."]) -- is "listener" a good choice?
+    , ("lo tavla ku pendo mi", ["The speaker is my friend.", "The speakers are my friends."]) -- is "speaker" a good choice? maybe it implies voice...
     ]
 
 translations2 :: [Translation]
 translations2 = translations1_nice ++ translations2_nice ++
     [ ("mi se zdani", ["I have a house."])
-    , ("lo prenu ku tavla lo gerku ku", ["A person is talking to a dog."])
+    , ("lo prenu ku tavla lo gerku ku", ["A person is talking to a dog.", "The person talks to dogs."])
     , ("lo gerku ku nelci lo mlatu ku", ["The dog likes the cat."])
-    , ("lo zdani ku melbi", ["The house is beautiful!"])
-    , ("mi te dunda lo mlatu ku", ["I was given a cat."])
+    , ("lo zdani ku melbi", ["The house is beautiful!", "The houses are beautiful!"])
+    , ("mi te dunda lo mlatu ku", ["I was given a cat.", "We were given a cat.", "We were given cats.", "Somebody gave me a cat.", "Somebody gave us a cat.", "Somebody gave us cats."])
     , ("mi ctuca do", ["I will teach you."])
     , ("mi se ctuca", ["Somebody teached me."])
     ]
 
 translations3 :: [Translation]
 translations3 =
-    [ ("lo prenu ku sutra tavla", ["The person talks quickly."])
+    [ ("lo prenu ku sutra tavla", ["The person talks quickly.", "The person is talking quickly.", "A person is talking quickly."])
     ]
 
 -------- Exercises
