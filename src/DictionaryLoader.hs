bd954f42eb0b197092e20a55e41f2bb09eb35138
Jonathan Queiroz <dev@johnjq.com>
2016-05-11 15:59:47 -0300

    Slightly improve the sumti places base.

--- src/DictionaryLoader.hs
+++ src/DictionaryLoader.hs
@@ -79,7 +79,7 @@ englishSumtiPlacesBase :: M.Map T.Text [T.Text]
 englishSumtiPlacesBase = M.fromList
     [ ("tavla", ["speaker", "listener", "subject", "language"])
     , ("dunda", ["donor", "gift", "recipient"])
-    , ("ctuca", ["instructor", "audience", "ideas/methods", "subject", "teaching method"])
+    , ("ctuca", ["instructor", "audience/student(s)", "ideas/methods", "subject", "teaching method"]) --TODO: maintain student(s) here?
     , ("citka", ["consumer", "aliment"])
     , ("klama", ["traveler", "destination", "origin", "route", "means/vehicle"])
     , ("bridi", ["predicate relationship", "relation", "arguments"])
