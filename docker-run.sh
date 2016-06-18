bce95e70a6815e9dc625222fd7bad8730c41315c
Jonathan Queiroz <dev@johnjq.com>
2016-06-17 23:06:53 -0300

    Slightly script "docker-run.sh".

--- docker-run.sh
+++ docker-run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 set -e
-
 ./docker-stop.sh $*
+
+echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Starting containers..."
 if [ -z "$DOCKER_OPTS" ]; then
+    echo -ne "\t"
     docker $DOCKER_OPTS run -p 80:8000/tcp -it --name lojto-server lojto-server
 else
+    echo -ne "\t"
     docker $DOCKER_OPTS run -d --name lojto-server lojto-server
 fi
+
+echo ""
