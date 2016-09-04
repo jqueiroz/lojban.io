45155e3fd9f95d9848a3d41b3dc1528d552cacb2
Jonathan Queiroz <dev@johnjq.com>
2016-09-04 18:54:40 -0300

    Handle a missing case in the sentence cannonicalizer.

--- src/Courses/Util/Sentences.hs
+++ src/Courses/Util/Sentences.hs
@@ -147,6 +147,9 @@ cannonicalizeArgumentInternally (ZG.BRIVLA b) = Right $ T.pack b
 cannonicalizeArgumentInternally (ZG.NU (ZG.Init init) text (ZG.Term term)) = do
     x <- cannonicalizeText text
     return . T.unwords $ [T.pack init, displayCannonicalBridi x, T.pack term]
+cannonicalizeArgumentInternally (ZG.NU (ZG.Init init) text (ZG.NT)) = do
+    x <- cannonicalizeText text
+    return . T.unwords $ [T.pack init, displayCannonicalBridi x, T.pack "kei"]
 cannonicalizeArgumentInternally (ZG.Prefix (ZG.SE se) b) = T.append (T.pack $ se ++ " ") <$> cannonicalizeArgumentInternally b
 cannonicalizeArgumentInternally _ = Left "unrecognized pattern in function cannonicalizeArgumentInternally"
 
