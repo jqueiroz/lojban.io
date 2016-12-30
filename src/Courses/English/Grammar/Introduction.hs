2ebc4bdf589575eca633bf6a51b106b1ca164d9f
Jonathan Queiroz <dev@johnjq.com>
2016-12-30 14:17:37 -0300

    Add some new translations.

--- src/Courses/English/Grammar/Introduction.hs
+++ src/Courses/English/Grammar/Introduction.hs
@@ -166,12 +166,18 @@ translations3 =
     , (["mi tavla fi lo nu do se zdani kei ku"], ["I talked about you having a house."])
     , (["do tavla fi lo nu lo mlatu ku nelci lo gerku ku kei ku"], ["You talked about cats liking dogs."])
     , (["do tavla fi lo nu lo gerku ku nelci lo mlatu ku kei ku"], ["You talked about dogs liking cats."])
+    , (["do tavla mi lo nu lo gerku ku nelci lo mlatu ku kei ku"], ["You talked to me about dogs liking cats."])
+    , (["do tavla mi lo nu lo mlatu ku nelci lo gerku ku kei ku"], ["You talked to me about cats liking dogs."])
     , (["mi nelci lo nu tavla do kei ku", "mi nelci lo nu mi tavla do kei ku"], ["I like to talk to you."])
     , (["do nupre lo nu dunda lo mlatu ku kei ku", "do nupre lo nu do dunda lo mlatu ku kei ku"], ["You promised to donate the cat.", "You promised to donate the cats."])
     , (["do nupre lo nu dunda lo gerku ku kei ku", "do nupre lo nu do dunda lo gerku ku"], ["You promised to donate the dog.", "You promised to donate the dogs."])
     , (["do nupre lo nu dunda lo mlatu ku mi kei ku", "do nupre lo nu do dunda lo mlatu ku mi kei ku"], ["You promised to donate the cat to me.", "You promised to donate the cats to me."])
     , (["do nupre lo nu dunda lo gerku ku mi kei ku", "do nupre lo nu do dunda lo gerku ku mi kei ku"], ["You promised to donate the dog to me.", "You promised to donate the dogs to me."])
+    , (["do nupre lo nu dunda lo mlatu ku kei ku mi", "do nupre lo nu do dunda lo mlatu ku kei ku mi"], ["You promised me to donate the cat.", "You promised me to donate the cats."])
+    , (["do nupre lo nu dunda lo gerku ku kei ku mi", "do nupre lo nu do dunda lo gerku ku kei ku mi"], ["You promised me to donate the dog.", "You promised me to donate the dogs."])
     , (["do nupre lo nu do pendo kei ku"], ["You promised to be friendly."])
+    , (["do nupre fi mi"], ["You promised me.", "You promised us."])
+    , (["mi nupre fi do"], ["I promised you."])
     ]
 
 translations4 :: [Translation]
