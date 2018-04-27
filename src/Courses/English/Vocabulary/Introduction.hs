bbd2b92f458924799a07ea3f830a7cc45b5bcd82
Jonathan Queiroz <dev@johnjq.com>
2018-04-26 21:33:50 -0300

    Add skeleton of vocabulary exercises

---
+++ src/Courses/English/Vocabulary/Introduction.hs
@@ -0,0 +1,41 @@
+wordlist1 =
+    [ "nupre"
+    , "sanga"
+    , "sidju"
+    ]
+
+sentences1 =
+    [ (["mi (nupre) lo ka na (sanga)"], ["I promise not to sing."])
+    , (["mi (nupre) lo nu (sidju) do", "mi (nupre) lo nu mi (sidju) do"], ["I promised to help you."])
+    , (["mi (nupre) lo ka (troci)"], ["I promise I'll try."])
+    , (["lo se (nupre) cu ka se (nupre)"], ["A promise is a promise."])
+    , (["mi (jinvi) lo du'u do pu (mutce) (tatpi)"], ["I thought that you were very tired."])
+    , (["mi pu (jinvi) lo du'u na (klama)"], ["I thought they wouldn't come."])
+    , (["la lojban cu (mutce) (melbi)"], ["Lojban is very beautiful."])
+    , (["lo ka (pilno) lo (vlaste) cu (frili)"], ["Using dictionaries is easy."])
+    , (["mi (jinvi) lo du'u la lojban cu (zabna)"], ["I think that lojban is cool."])
+    , (["xu do (kakne) lo ka (sutra) (tavla)", "Are you able to talk quickly?"])
+    , (["mi na jinvi lo nu ra ba klama", "I don't think he will come."])
+    , (["xu do ba klama", "Are you coming?"])
+    , (["mi na jinvi lo du'u ra vamji", "I don't think it's worth it."])
+    , (["mi pu jinvi lo nu do pu se catra", "I thought you had been murdered."])
+    ]
+
+simple = 
+    [ (["mi na djuno"], ["I don't know."])
+    , (["mi na djica"], ["I don't want."])
+    , (["mi ca tavla do"], ["I am talking to you."])
+    , (["do drani"], ["You are correct."])
+    ]
+
+tenses =
+    [ (["mi na jinvi lo nu ra klama"], ["I don't think he will come."])
+    ]
+
+harder =
+    [ (["mi (jinvi) lo nu (gasnu) lo se (nupre) cu (vajni)"], ["I think that keeping promises is important."])
+    , (["mi (nupre) lo nu na ba (rapli)"], ["I promise that this won't happen again."])
+    , (["do pu (nupre) fi mi"], ["You promised me."])
+    , (["mi jinvi da"], ["I have an opinion."])
+    , (["mi na jinvi lo du'u da djuno lo danfu"], ["I don't think that anybody knows the answer."])
+    ]
