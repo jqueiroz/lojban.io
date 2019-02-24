5687277f0757dbf599bd4e63ba5b54691877dbeb
Jonathan Queiroz <dev@johnjq.com>
2019-02-24 06:19:34 -0300

    Add TODO

--- src/Courses/English/Grammar/Introduction.hs
+++ src/Courses/English/Grammar/Introduction.hs
@@ -533,6 +533,7 @@ questionExercises3 = generateFillingBlanksExercise ["mo", "ma"] $ combineFunctio
 -- TODO: more examples using du'u
 -- consider using: morji, ciksi, jijnu (useful for teaching du'u)
 -- TODO: add sentences using promisee
+-- TODO: replace some occurrences of "happy" with "glad"?
 -- Are events vs facts being used correctly?
 translations4_nu :: TranslationGenerator
 translations4_nu = combineFunctions [(2, gleki), (1, tavla), (2, nupre)] where
