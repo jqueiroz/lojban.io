28e660848656e6ae9408f3d5b5fe1e18b531ce74
Jonathan Queiroz <dev@johnjq.com>
2019-03-11 22:41:34 -0300

    Setup hlint

--- lint.sh
+++ lint.sh
@@ -6,7 +6,24 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+# Haskell
+echo "===> Haskell"
+echo "Running hlint..."
+
+ALL_FILES_OKAY="true"
+for file in $(hlint -j8 --json src | jq --raw-output ".[].file" | uniq); do
+    ALL_FILES_OKAY="false"
+    echo -e "${RED}$file${NC}"
+done
+
+if [ "$ALL_FILES_OKAY" == "true" ]; then
+    echo -e "${GREEN}All files passed.${NC}"
+else
+    echo -e "Code style issues found in the above file(s). Forgot to check hlint?"
+fi
+
 # Javascript
+echo ""
 echo "===> Javascript"
 echo "Checking formatting..."
 
