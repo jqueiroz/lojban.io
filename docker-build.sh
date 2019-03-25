00c22895d838c7765bc385925bb84c347b71e455
Jonathan Queiroz <dev@johnjq.com>
2019-03-24 22:29:53 -0700

    Setup static build

--- docker-build.sh
+++ docker-build.sh
@@ -4,7 +4,7 @@ set -e
 echo ">>>>>>>>>>>>>> lojto"
 if [ -z "$1" ]; then
     ./compile-less.sh
-    stack build && cp .stack-work/dist/x86_64-*/Cabal*/build/lojto/lojto .docker-binary
+    stack build && cp .stack-work/install/x86_64-linux-nix/lts-12.26/*/bin/lojto .docker-binary
     docker $DOCKER_OPTS build -t lojto-server .
     echo ""
     echo ""
