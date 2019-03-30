62dcd1fa695950f4640fe9ca21bbb3ab99fcdac3
Jonathan Queiroz <dev@johnjq.com>
2019-03-29 23:30:15 -0700

    Start writing tests

---
+++ tests/Language.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Test.Hspec
+import Courses.Util.Sentences (basicSentenceCanonicalizer)
+import Data.Either
+import qualified Data.Text as T
+
+validateSentences :: [(T.Text, T.Text)] -> IO ()
+validateSentences sentences = map basicSentenceCanonicalizer originalSentences `shouldBe` canonicalSentences where
+  originalSentences = map fst sentences
+  canonicalSentences = map (Right . snd) sentences
+
+main :: IO ()
+main = hspec $ do
+    describe "Basic sentence canonicalizer" $ do
+      it "supports SE" $ do
+        validateSentences
+            [ ("mi se nelci lo mlatu", "lo mlatu ku nelci mi")
+            , ("lo mlatu cu te tavla", "tavla zo'e lo mlatu ku")
+            ]
+      it "supports FA" $ do
+        validateSentences
+            [ ("mi tavla fi lo mlatu", "mi tavla zo'e lo mlatu ku")
+            , ("mi ctuca fo lo bangu", "mi ctuca zo'e zo'e lo bangu ku")
+            ]
