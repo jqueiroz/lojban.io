0fbb24a1abb9570a0f86e7b3f12824a6476ea50e
Jonathan Queiroz <dev@johnjq.com>
2019-04-06 21:16:02 -0700

    Add a new test

--- tests/Language.hs
+++ tests/Language.hs
@@ -43,3 +43,7 @@ main = hspec $ do
         --validateSentences
             --[ ("do nelci xu lo mlatu", "xu do nelci lo mlatu ku")
             --]
+      it "supports complex sentences" $ do
+        validateSentences
+            [ ("mi nupre lo nu pilno lo skami poi do dunda ke'a mi", "mi nupre lo nu pilno lo skami ku poi do dunda ke'a mi ku'o kei ku")
+            ]
