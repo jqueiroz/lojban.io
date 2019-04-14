d9bfd31331da9c7afde2ad81bc53076da4411b4b
Jonathan Queiroz <dev@johnjq.com>
2019-04-14 12:06:00 -0700

    Update script "docker-build.sh"

--- dist/docker-build.sh
+++ dist/docker-build.sh
@@ -2,15 +2,6 @@
 set -e
 
 echo ">>>>>>>>>>>>>> lojto"
-if [ -z "$1" ]; then
-    ./compile-less.sh
-    stack build && cp .stack-work/install/x86_64-linux-nix/lts-12.26/*/bin/lojto .docker-binary
-    docker $DOCKER_OPTS build -t lojto-server .
-    echo ""
-    echo ""
-elif [ "$1" == "from-source" ]; then
-    rm -f .docker-binary
-    docker $DOCKER_OPTS build -t lojto-server -f Dockerfile2 .
-    echo ""
-    echo ""
-fi
+docker $DOCKER_OPTS build -t lojto-server -f Dockerfile .
+echo ""
+echo ""
