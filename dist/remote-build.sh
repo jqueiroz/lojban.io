98468892802058a3840105efb795b292e3fd5dbb
Jonathan Queiroz <dev@johnjq.com>
2019-07-01 23:40:49 -0700

    Add hashbang to some scripts

--- dist/remote-build.sh
+++ dist/remote-build.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
 export DOCKER_OPTS="-H :5054"
 ./docker-build.sh $*
--- dist/remote-tunnel.sh
+++ dist/remote-tunnel.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+set -e
+
 ssh root@files.johnjq.com -L 5054:127.0.0.1:2375 -N
