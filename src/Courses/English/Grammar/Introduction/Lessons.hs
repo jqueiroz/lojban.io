c0b9832d20d785dfbf289ceee31d2c8fa387850e
Jonathan Queiroz <dev@johnjq.com>
2019-07-06 08:43:55 -0700

    Document Introduction.Lessons

--- src/Courses/English/Grammar/Introduction/Lessons.hs
+++ src/Courses/English/Grammar/Introduction/Lessons.hs
@@ -1,36 +1,46 @@
+-- | This module defines the course lessons.
 module Courses.English.Grammar.Introduction.Lessons  where
 
 import Core
 import Courses.English.Grammar.Introduction.Documents
 import Courses.English.Grammar.Introduction.Exercises
 
--------- Lessons
+-- | First lesson: Basics 1.
 lesson1 :: LessonBuilder
 lesson1 dictionary = Lesson "Basics 1" (exercises1 dictionary) plan1
 
+-- | Second lesson: Basics 2.
 lesson2 :: LessonBuilder
 lesson2 dictionary = Lesson "Basics 2" (exercises2 dictionary) plan2
 
+-- | Third lesson: Questions 1.
 lesson3 :: LessonBuilder
 lesson3 dictionary = Lesson "Questions 1" (exercises3 dictionary) plan3
 
+-- | Fourth lesson: Abstractions 1.
 lesson4 :: LessonBuilder
 lesson4 dictionary = Lesson "Abstractions 1" (exercises4 dictionary) plan4
 
+-- | Fifth lesson: Terminator elision.
 lesson5 :: LessonBuilder
 lesson5 dictionary = Lesson "Terminator elision" (exercises5 dictionary) plan5
 
+-- | Sixth lesson: Checkpoint (Lessons 1--5).
 checkpoint1to5 :: LessonBuilder
 checkpoint1to5 dictionary = Lesson "Checkpoint: Lessons 1–5" (exercises1to5 dictionary) plan1to5
 
+-- | Seventh lesson: Relative clauses.
 lesson7 :: LessonBuilder
 lesson7 dictionary = Lesson "Relative clauses" (exercises7 dictionary) plan7
 
+-- | Eighth lesson: Linked sumti.
 lesson8 :: LessonBuilder
 lesson8 dictionary = Lesson "Linked sumti" (exercises8 dictionary) plan8
 
+-- | Nineth lesson: Sumtcita
 lesson9 :: LessonBuilder
 lesson9 dictionary = Lesson "Sumtcita" (exercises9 dictionary) plan9
 
+-- | Tenth lesson: Tenses 1
 lesson10 :: LessonBuilder
 lesson10 dictionary = Lesson "Tenses 1" (exercises10 dictionary) plan10
