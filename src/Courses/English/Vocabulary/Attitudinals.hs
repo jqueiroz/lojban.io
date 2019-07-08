2bb20bebf7cc18b1574caf5a851e6e725d30e4bb
Jonathan Queiroz <dev@johnjq.com>
2019-07-08 02:29:27 -0700

    Write notes about attitudinals course

--- src/Courses/English/Vocabulary/Attitudinals.hs
+++ src/Courses/English/Vocabulary/Attitudinals.hs
@@ -1,2 +1,20 @@
 -- Interesting exercise: "Rewrite using attitudinals"
 -- Example: "mi djica lo nu mi citka" -> ".au mi citka"
+
+-- First lesson:
+--   coi, doi, do'u
+--   co'o
+--   mi'e
+--   je'e, ki'e
+--   go'i, na go'i
+-- Second lesson:
+--   fi'i
+--   pe'u
+--   je'e
+-- Third lesson:
+--   ju'i
+
+-- Others:
+--   .ui penmi do
+--   .a'o xamgu nicte
+--   .a'o xamgu djedi
