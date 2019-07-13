fb0d0f92295a66ccc76cd7facf3e0f7bd75852a8
Jonathan Queiroz <dev@johnjq.com>
2019-07-12 22:36:51 -0700

    Improve sentence comparer

--- src/Courses/English/Grammar/Introduction/Strategies.hs
+++ src/Courses/English/Grammar/Introduction/Strategies.hs
@@ -22,6 +22,9 @@ import qualified Data.Text as T
 --     * TODO: this still needs to be implemented
 --
 -- * Association words ("pe", "ne", "po" and "po'e") are interchangeable.
+-- * The quote delimiters "lu\/li'u" and "lo'u/le'u" are interchangeable.
+--
+--     * Conveniently, it is nonetheless enforced that "lu\/li'u" must only be used around grammatical text, as otherwise the canonicalization fails due to a parsing failure.
 sentenceComparer :: SentenceComparer
 sentenceComparer x y = (length xs == length ys) && (all wordComparer $ zip xs ys) where
     xs = T.words x
@@ -30,6 +33,8 @@ sentenceComparer x y = (length xs == length ys) && (all wordComparer $ zip xs ys
     isAssociationWord x = x `elem` [ "pe", "ne", "po", "po'e" ]
     wordComparer :: (T.Text, T.Text) -> Bool
     wordComparer (x, y) = (wordComparer' x y) || (wordComparer' y x) || (isAssociationWord x && isAssociationWord y)
+    wordComparer' "lu" "lo'u" = True
+    wordComparer' "li'u" "le'u" = True
     wordComparer' "nu" "su'u" = True
     wordComparer' "du'u" "su'u" = True
     wordComparer' x y = x == y
