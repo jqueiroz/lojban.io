c5aa11093e5611c49074acf88fad3ea4af6669cf
Jonathan Queiroz <dev@johnjq.com>
2019-07-28 13:14:53 -0700

    Move translation loaders to separate module

--- src/Courses/English/Vocabulary/Brivla/Translations.hs
+++ src/Courses/English/Vocabulary/Brivla/Translations.hs
@@ -5,32 +5,8 @@
 module Courses.English.Vocabulary.Brivla.Translations where
 
 import Core
+import Courses.Framework.TranslationLoaders (loadTranslationsByExpressionFromYamlText)
 import Data.FileEmbed (embedStringFile)
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as TE
-import qualified Data.Map as M
-import qualified Data.Yaml as Y
-
--- * Auxiliar functions
-loadTranslationsByExpressionFromYamlText :: T.Text -> TranslationsByExpression
-loadTranslationsByExpressionFromYamlText yamlText = M.assocs $ M.map handleExpression yamlData where
-    yamlData :: M.Map T.Text [M.Map T.Text [T.Text]]
-    Right yamlData = Y.decodeEither $ TE.encodeUtf8 yamlText
-    handleExpression :: [M.Map T.Text [T.Text]] -> [Translation]
-    handleExpression = map handleTranslation
-    handleTranslation :: M.Map T.Text [T.Text] -> Translation
-    handleTranslation dict = (dict M.! "lojban_sentences", dict M.! "translated_sentences")
-
-saveTranslationsByExpressionToYamlText :: TranslationsByExpression -> T.Text
-saveTranslationsByExpressionToYamlText translationsByExpression = TE.decodeUtf8 $ Y.encode yamlData where
-    yamlData :: M.Map T.Text [M.Map T.Text [T.Text]]
-    yamlData = M.map encodeExpression $ M.fromList translationsByExpression
-    encodeExpression :: [Translation] -> [M.Map T.Text [T.Text]]
-    encodeExpression = map encodeTranslation
-    encodeTranslation :: Translation -> M.Map T.Text [T.Text]
-    encodeTranslation (lojban_sentences, english_sentences) = M.fromList $ [("lojban_sentences", lojban_sentences), ("translated_sentences", english_sentences)]
-
--- * Translations
 
 -- | Translations for the corresponding lesson.
 translations01 :: TranslationsByExpression
---
+++ src/Courses/Framework/TranslationLoaders.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | This module provides utilities for loading translations from files.
+module Courses.Framework.TranslationLoaders
+( loadTranslationsByExpressionFromYamlText
+) where
+
+import Core
+import qualified Data.Map as M
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.Yaml as Y
+
+-- | Loads 'TranslationsByExpression' from yaml code.
+loadTranslationsByExpressionFromYamlText :: T.Text -> TranslationsByExpression
+loadTranslationsByExpressionFromYamlText yamlText = M.assocs $ M.map handleExpression yamlData where
+    yamlData :: M.Map T.Text [M.Map T.Text [T.Text]]
+    Right yamlData = Y.decodeEither $ TE.encodeUtf8 yamlText
+    handleExpression :: [M.Map T.Text [T.Text]] -> [Translation]
+    handleExpression = map handleTranslation
+    handleTranslation :: M.Map T.Text [T.Text] -> Translation
+    handleTranslation dict = (dict M.! "lojban_sentences", dict M.! "translated_sentences")
+
+-- | Saves 'TranslationsByExpression' to yaml code.
+saveTranslationsByExpressionToYamlText :: TranslationsByExpression -> T.Text
+saveTranslationsByExpressionToYamlText translationsByExpression = TE.decodeUtf8 $ Y.encode yamlData where
+    yamlData :: M.Map T.Text [M.Map T.Text [T.Text]]
+    yamlData = M.map encodeExpression $ M.fromList translationsByExpression
+    encodeExpression :: [Translation] -> [M.Map T.Text [T.Text]]
+    encodeExpression = map encodeTranslation
+    encodeTranslation :: Translation -> M.Map T.Text [T.Text]
+    encodeTranslation (lojban_sentences, english_sentences) = M.fromList $ [("lojban_sentences", lojban_sentences), ("translated_sentences", english_sentences)]
