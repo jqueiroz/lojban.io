995acc0f7605e26339fb9c5e558f6e0a78467787
Jonathan Queiroz <dev@johnjq.com>
2019-07-28 14:09:22 -0700

    Implement translation loader

--- src/Courses/English/Grammar/Crash/Translations.hs
+++ src/Courses/English/Grammar/Crash/Translations.hs
@@ -1,30 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 -- | This module provides translations (and exercises relying on translations) for each of the course lessons.
 module Courses.English.Grammar.Crash.Translations where
 
 import Core
-import Util (generatorFromList)
+import Courses.Framework.TranslationLoaders (loadTranslationGeneratorFromYamlCode)
+import Data.FileEmbed (embedStringFile)
 
--- * Lesson 1: Basics 1
-
--- | Translations for the first lesson.
+-- | Translations for the corresponding lesson.
 translations01 :: TranslationGenerator
-translations01 = generatorFromList
-    [ (["lo mlatu cu pinxe lo ladru"], ["Cats drink milk."])
-    , (["lo plise cu kukte"], ["Apples are tasty."])
-    , (["karce"], ["Car!"])
-    , (["carvi"], ["It is raining."])
-    ]
+translations01 = loadTranslationGeneratorFromYamlCode $(embedStringFile "courses/english/grammar/crash/translations/01.yaml")
 
--- * Lesson 2: Basics 2
-
--- | Translations for the second lesson.
+-- | Translations for the corresponding lesson.
 translations02 :: TranslationGenerator
-translations02 = generatorFromList
-    [ (["mi pinxe"], ["I drink."])
-    , (["mi pinxe"], ["You drink."])
-    , (["ti ladru"], ["This is some milk."])
-    , (["(tu|ta) mlatu"], ["That is a cat."])
-    , (["do citka lo plise"], ["You like apples."])
-    ]
+translations02 = loadTranslationGeneratorFromYamlCode $(embedStringFile "courses/english/grammar/crash/translations/02.yaml")
--- src/Courses/Framework/TranslationLoaders.hs
+++ src/Courses/Framework/TranslationLoaders.hs
@@ -2,14 +2,32 @@
 
 -- | This module provides utilities for loading translations from files.
 module Courses.Framework.TranslationLoaders
-( loadTranslationsByExpressionFromYamlCode
+( loadTranslationsFromYamlCode
+, loadTranslationGeneratorFromYamlCode
+, loadTranslationsByExpressionFromYamlCode
 ) where
 
 import Core
+import Courses.Framework.TranslationUtils (expandTranslation)
+import Util (generatorFromList)
 import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.Yaml as Y
+
+-- | Loads translations from yaml code.
+loadTranslationsFromYamlCode :: T.Text -> [Translation]
+loadTranslationsFromYamlCode yamlCode = map loadTranslationFromYamlData yamlData where
+    yamlData :: [M.Map T.Text [T.Text]]
+    Right yamlData = Y.decodeEither $ TE.encodeUtf8 yamlCode
+
+-- | Loads translations from yaml data.
+loadTranslationFromYamlData :: M.Map T.Text [T.Text] -> Translation
+loadTranslationFromYamlData yamlData = expandTranslation $ (yamlData M.! "lojban_sentences", yamlData M.! "translated_sentences")
+
+-- | Loads 'TranslationGenerator' from yaml code.
+loadTranslationGeneratorFromYamlCode :: T.Text -> TranslationGenerator
+loadTranslationGeneratorFromYamlCode = generatorFromList . loadTranslationsFromYamlCode
 
 -- | Loads 'TranslationsByExpression' from yaml code.
 loadTranslationsByExpressionFromYamlCode :: T.Text -> TranslationsByExpression
@@ -17,9 +35,7 @@ loadTranslationsByExpressionFromYamlCode yamlCode = M.assocs $ M.map handleExpre
     yamlData :: M.Map T.Text [M.Map T.Text [T.Text]]
     Right yamlData = Y.decodeEither $ TE.encodeUtf8 yamlCode
     handleExpression :: [M.Map T.Text [T.Text]] -> [Translation]
-    handleExpression = map handleTranslation
-    handleTranslation :: M.Map T.Text [T.Text] -> Translation
-    handleTranslation dict = (dict M.! "lojban_sentences", dict M.! "translated_sentences")
+    handleExpression = map loadTranslationFromYamlData
 
 -- | Saves 'TranslationsByExpression' to yaml code.
 saveTranslationsByExpressionToYamlText :: TranslationsByExpression -> T.Text
