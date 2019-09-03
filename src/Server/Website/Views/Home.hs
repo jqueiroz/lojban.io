35f35f606b75f8847acb3ad07d8e686159c6db45
Jonathan Queiroz <dev@johnjq.com>
2019-09-02 19:31:28 -0700

    Update homepage design

--- src/Server/Website/Views/Home.hs
+++ src/Server/Website/Views/Home.hs
@@ -29,15 +29,7 @@ displayHome userIdentityMaybe =
             displayTopbar userIdentityMaybe TopbarHome
             H.div B.! A.class_ (H.textValue "main") $ do
                 H.div B.! A.class_ (H.textValue "header") $ do
-                    H.h1 $ do
-                        H.span B.! A.class_ (H.textValue "learn") $ H.toHtml ("Learn " :: T.Text)
-                        H.span B.! A.class_ (H.textValue "logically") $ H.toHtml ("logically" :: T.Text)
-                    H.p $ H.toHtml ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor." :: String)
-                    H.div B.! A.class_ (H.textValue "buttons") $ do
-                        H.a (H.toHtml ("Courses" :: T.Text))
-                            B.! A.href (H.textValue "/courses")
-                        H.a (H.toHtml ("Decks" :: T.Text))
-                            B.! A.href (H.textValue "/decks")
+                    displayHeader2
                 H.div B.! A.class_ (H.textValue "courses") $ do
                     H.h2 $ H.toHtml ("Courses" :: T.Text)
                     H.div B.! A.class_ (H.stringValue "course-list") $ do
@@ -53,6 +45,66 @@ displayHome userIdentityMaybe =
                         displayDeck ("/decks/contextualized-brivla", Decks.English.ContextualizedBrivla.deck)
                         displayDeck ("/decks/contextualized-brivla", Decks.English.ContextualizedBrivla.deck)
                 displayFooter
+
+displayHeader1 :: H.Html
+displayHeader1 = do
+    displayWhat
+    displayWhy
+    displayLearn
+
+displayHeader2 :: H.Html
+displayHeader2 = do
+    displaySpeak
+    displayWhy
+
+-- TODO: url to official lojban website
+displaySpeak :: H.Html
+displaySpeak = do
+    H.div B.! A.class_ (H.textValue "speak-lojban") $ do
+        H.h1 $ do
+            H.span B.! A.class_ (H.textValue "speak") $ H.toHtml ("Speak " :: T.Text)
+            H.span B.! A.class_ (H.textValue "logically") $ H.toHtml ("logically" :: T.Text)
+        H.p $ H.toHtml ("Lojban is a carefully constructed spoken language. It has been built for over 50 years by dozens of workers and hundreds of supporters." :: T.Text)
+        H.p $ H.toHtml ("Lojban's grammar is based on simple rules, and its linguistic features are inspired by predicate logic." :: T.Text)
+        H.div B.! A.class_ (H.textValue "buttons") $ do
+            H.a (H.toHtml ("Courses" :: T.Text))
+                B.! A.href (H.textValue "/courses")
+            H.a (H.toHtml ("Decks" :: T.Text))
+                B.! A.href (H.textValue "/decks")
+
+displayWhat :: H.Html
+displayWhat = do
+    H.div B.! A.class_ (H.textValue "what-lojban") $ do
+        H.h1 $ H.toHtml ("What is Lojban?" :: T.Text)
+        H.p $ H.toHtml ("Lojban is a carefully constructed spoken language. It has been built for over 50 years by dozens of workers and hundreds of supporters." :: T.Text)
+        H.p $ H.toHtml ("Lojban's grammar is based on simple rules, and its linguistic features are inspired by predicate logic." :: T.Text)
+
+displayWhy :: H.Html
+displayWhy = do
+    H.div B.! A.class_ (H.textValue "why-lojban") $ do
+        H.h1 $ H.toHtml ("Why Lojban" :: T.Text)
+        H.p $ H.toHtml ("Lojban means different things to different people:" :: T.Text)
+        H.ul $ do
+            H.li $ H.toHtml ("a new perspective on languages;" :: T.Text)
+            H.li $ H.toHtml ("a challenging way to expand their minds or discipline their thoughts;" :: T.Text)
+            H.li $ H.toHtml ("an entertaining medium to communicate with friends or create art;" :: T.Text)
+            H.li $ H.toHtml ("a linguistic curiosity – a test-bed for language experimentation;" :: T.Text)
+            H.li $ H.toHtml ("a domain for exploring the intersection of human language and software." :: T.Text)
+        --H.p $ H.toHtml ("What will Lojban mean for you?" :: T.Text)
+
+displayLearn :: H.Html
+displayLearn = do
+    H.div B.! A.class_ (H.textValue "learn-logically") $ do
+        H.h1 $ do
+            H.span B.! A.class_ (H.textValue "learn") $ H.toHtml ("Learn " :: T.Text)
+            H.span B.! A.class_ (H.textValue "logically") $ H.toHtml ("logically" :: T.Text)
+        H.p $ H.toHtml ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor." :: String)
+        H.div B.! A.class_ (H.textValue "buttons") $ do
+            H.a (H.toHtml ("Courses" :: T.Text))
+                B.! A.href (H.textValue "/courses")
+            H.a (H.toHtml ("Decks" :: T.Text))
+                B.! A.href (H.textValue "/decks")
+
 
 displayCourse :: (T.Text, Course) -> H.Html
 displayCourse (url, course) = do
