fb7493a3a2d47ae7b1e89c6592b08cb1b68f5c1d
sylviot <sylvio.tavares@hotmail.com>
2019-09-09 23:21:07 -0400

    Testing typescript to js

--- assets/typescript/home.ts
+++ assets/typescript/home.ts
@@ -1,37 +1,40 @@
-"use strict";
-exports.__esModule = true;
-var $ = require("jquery");
-var setupCarousel = function (previousButton, nextButton, elements) {
+
+import * as $ from 'jquery';
+
+let setupCarousel = function(previousButton, nextButton, elements) {
     // TODO: dynamically compute the number of pages
     // TODO: implement animations
-    var numberOfElements = elements.length;
-    var numberOfElementsPerPage = 3;
-    var numberOfPages = Math.ceil(numberOfElements / numberOfElementsPerPage);
-    var displayPage = function (pageNumber) {
+    let numberOfElements = elements.length;
+    let numberOfElementsPerPage = 3;
+    let numberOfPages = Math.ceil(numberOfElements / numberOfElementsPerPage);
+
+    let displayPage = function(pageNumber) {
         elements.hide();
-        var indexOfFirstElement = pageNumber * numberOfElementsPerPage;
-        for (var i = 0; i < numberOfElementsPerPage; ++i) {
-            var indexOfCurrentElement = indexOfFirstElement + i;
+        let indexOfFirstElement = pageNumber * numberOfElementsPerPage;
+        for (let i = 0; i < numberOfElementsPerPage; ++i) {
+            let indexOfCurrentElement = indexOfFirstElement + i;
             if (indexOfCurrentElement < numberOfElements) {
                 $(elements[indexOfCurrentElement]).show();
             }
         }
-    };
+    }
     displayPage(0);
-    var currentPage = 0;
-    previousButton.click(function () {
+
+    let currentPage = 0;
+    previousButton.click(function() {
         currentPage += numberOfPages;
         currentPage -= 1;
         currentPage %= numberOfPages;
         displayPage(currentPage);
     });
-    nextButton.click(function () {
+    nextButton.click(function() {
         currentPage += 1;
         currentPage %= numberOfPages;
         displayPage(currentPage);
     });
 };
-$(document).ready(function () {
+
+$(document).ready(function() {
     setupCarousel($(".courses .previous"), $(".courses .next"), $(".courses .course"));
     setupCarousel($(".decks .previous"), $(".decks .next"), $(".decks .deck"));
 });
