8ca359b2f6202f81ef7de04d555268adda80f1ef
sylviot <sylvio.junior@globalweb.com.br>
2019-09-10 10:41:46 -0400

    Gulp with font-awesome and others configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,43 @@
 'use strict';
 
-// const FONTS_FILES = "./node_modules/font-awesome/fonts/*.*";
-// const SCSS_FILES = "./Resources/sass/*.scss";
-const SCRIPTS_FILES = "./Resources/scripts/*.js";
+const LESS_FILES = 'assets/less/**/*.less';
 const TYPESCRIPT_FILES = 'assets/typescript/**/*.ts';
+const FONT_FILES = [
+    './node_modules/bootstrap/fonts/*.*',
+    './node_modules/font-awesome/fonts/*.*'
+];
 const VENDORS_SCRIPTS_FILES = [
     './node_modules/jquery/dist/jquery.js',
     './node_modules/bootstrap/dist/js/bootstrap.min.js'
 ];
+const VENDORS_STYLES_FILES = [
+    './node_modules/bootstrap/dist/css/bootstrap.min.css',
+    './node_modules/font-awesome/css/font-awesome.min.css'
+];
 
 var gulp = require('gulp');
 var concat = require("gulp-concat");
+var less = require('gulp-less');
 var ts = require('gulp-typescript');
 var uglify = require("gulp-uglify");
 
-/*
-gulp.task("fonts",
-    function() {
-        return gulp.src(FONTS_FILES)
-            .pipe(gulp.dest("./Assets/fonts"));
-    });
-
-gulp.task("sass", function () {
-    return gulp.src(SCSS_FILES)
-      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-      .pipe(gulp.dest("./Assets/css"));
+/* TASK'S */
+gulp.task("fonts", function() {
+    return gulp.src(FONT_FILES)
+        .pipe(gulp.dest("./Assets/fonts"));
 });
-*/
 
 gulp.task("scripts", function () {
     return gulp.src(TYPESCRIPT_FILES)
         .pipe(ts())
         .pipe(gulp.dest("./static/scripts"));
+});
+
+gulp.task("styles", function(){
+    return gulp.src(LESS_FILES)
+        .pipe(less({
+        }))
+        .pipe(gulp.dest('./static/style'));
 });
 
 gulp.task("vendors:scripts", function () {
@@ -41,9 +47,15 @@ gulp.task("vendors:scripts", function () {
         .pipe(gulp.dest("./static/scripts"));
 });
 
-gulp.task('default', gulp.series('scripts', 'vendors:scripts'));
+gulp.task("vendors:styles", function() {
+    return gulp.src(VENDORS_STYLES_FILES)
+        .pipe(concat("vendors.css"))
+        .pipe(gulp.dest("./static/style"));
+});
+
+gulp.task('default', gulp.series('fonts', 'scripts', 'styles', 'vendors:scripts', 'vendors:styles'));
 
 gulp.task("watch", function () {
-    // gulp.watch(SCSS_FILES, gulp.series("sass"));
     gulp.watch(TYPESCRIPT_FILES, gulp.series("scripts"));
+    gulp.watch(LESS_FILES, gulp.series("styles"));
 });
