b7786394636d6fc68fc984c381e9a853db06e0c6
Jonathan Queiroz <dev@johnjq.com>
2019-09-11 20:18:33 -0700

    Move font import from html to css

--- haskell/src/Server/Website/Views/Core.hs
+++ haskell/src/Server/Website/Views/Core.hs
@@ -28,7 +28,7 @@ includeUniversalStylesheets = do
     -- TODO: consider removing
     includeInternalStylesheet "bootstrap.min.css"
     --includeExternalStylesheet "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
-    includeExternalStylesheet "https://fonts.googleapis.com/icon?family=Material+Icons"
+    --includeExternalStylesheet "https://fonts.googleapis.com/icon?family=Material+Icons"
 
 includeInternalStylesheet :: String -> H.Html
 includeInternalStylesheet = includeExternalStylesheet . ("/static/style/"++)
