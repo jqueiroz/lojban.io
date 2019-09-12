2c245d2826f0d18d59e67ef3b379adf187327b19
Jonathan Queiroz <dev@johnjq.com>
2019-09-11 22:03:00 -0700

    Rename directory: env -> environments

--- buildscripts/make-javascript.sh
+++ buildscripts/make-javascript.sh
@@ -7,7 +7,7 @@ cd "$DIR/.."
 
 # Function to run commands inside a Nix shell
 run_command(){
-    nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.03.tar.gz --pure "./buildscripts/env/javascript.nix" --run "$1"
+    nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.03.tar.gz --pure "./buildscripts/environments/javascript.nix" --run "$1"
 }
 
 if [[ -n "$LOJBAN_TOOL_BYPASS_NIX" ]]; then
