f2ba4329df32e3afc9cce9c36b733f37df9f4d81
Jonathan Queiroz <dev@johnjq.com>
2019-09-11 19:02:10 -0700

    Rename directory: dist -> virtualization

--- virtualization/intermediary-image/docker-build.sh
+++ virtualization/intermediary-image/docker-build.sh
@@ -3,7 +3,7 @@ set -e
 
 # Change directory to the project's root
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR/.."
+cd "$DIR/../.."
 
-# Display logs
-./dist/docker.sh logs lojban-dev-server
+# Build docker image
+docker build -t lojban-dependencies -f ./virtualization/intermediary-image/Dockerfile .
