5dce769622a1be06afc75df1b6ee75e02bab08f0
Jonathan Queiroz <dev@johnjq.com>
2020-08-16 21:36:57 -0700

    Add support for "lojban.io" domain

--- virtualization/prod-containers/letsencrypt/scripts/renew.sh
+++ virtualization/prod-containers/letsencrypt/scripts/renew.sh
@@ -5,4 +5,4 @@ letsencrypt/letsencrypt-auto certonly \
   --keep-until-expiring --agree-tos --webroot --expand --rsa-key-size 4096 \
   --email lojban@johnjq.com \
   --webroot-path /letsencrypt-challenge \
-  -d lojban.johnjq.com
+  -d lojban.johnjq.com -d lojban.io
