d3f218200a1655e90123cbdadfc340e74137b1bc
Jonathan Queiroz <dev@johnjq.com>
2020-08-17 21:33:53 -0700

    Improve design of deck page

--- haskell/src/Server/Website/Views/Deck.hs
+++ haskell/src/Server/Website/Views/Deck.hs
@@ -38,21 +38,22 @@ displayDeckHome userIdentityMaybe deck = do
                     H.div B.! A.class_ (H.textValue "header-bg") $ H.toHtml ("" :: T.Text)
                     displayDeckHomeHeader baseDeckUrl deck
                 H.div B.! A.class_ (H.textValue "body") $ do
-                    when (isJust $ deckLongDescription deck) $ do
-                        H.div B.! A.class_ (H.stringValue "deck-about") $ do
-                            H.h2 $ H.toHtml ("About this deck" :: String)
-                            H.div $ do
-                                fromRight . P.runPure . PWH.writeHtml5 P.def $ fromJust (deckLongDescription deck)
-                    H.div B.! A.class_ (H.stringValue "deck-manage") $ do
-                        -- TOOD: Section: "Manage your cards"
-                        H.h2 $ H.toHtml ("Manage your cards" :: T.Text)
-                        -- TOOD: Some hints as well (eg, "generally you do not need to disable cards that you already know. If we think you have mastered a card, we will display it less frequently so that you can focus on...")
-                        H.div B.! A.class_ (H.stringValue "deck-cards") $ H.toHtml ("" :: T.Text)
-                    when (isJust $ deckCredits deck) $ do
-                        H.div B.! A.class_ (H.stringValue "deck-credits") $ do
-                            H.h2 $ H.toHtml ("Credits" :: String)
-                            H.div $ do
-                                fromRight . P.runPure . PWH.writeHtml5 P.def $ fromJust (deckCredits deck)
+                    H.div B.! A.class_ (H.textValue "deck-contents") $ do
+                        when (isJust $ deckLongDescription deck) $ do
+                            H.div B.! A.class_ (H.stringValue "deck-about") $ do
+                                H.h2 $ H.toHtml ("About this deck" :: String)
+                                H.div $ do
+                                    fromRight . P.runPure . PWH.writeHtml5 P.def $ fromJust (deckLongDescription deck)
+                        H.div B.! A.class_ (H.stringValue "deck-manage") $ do
+                            -- TOOD: Section: "Manage your cards"
+                            H.h2 $ H.toHtml ("Manage your cards" :: T.Text)
+                            -- TOOD: Some hints as well (eg, "generally you do not need to disable cards that you already know. If we think you have mastered a card, we will display it less frequently so that you can focus on...")
+                            H.div B.! A.class_ (H.stringValue "deck-cards") $ H.toHtml ("" :: T.Text)
+                        when (isJust $ deckCredits deck) $ do
+                            H.div B.! A.class_ (H.stringValue "deck-credits") $ do
+                                H.h2 $ H.toHtml ("Credits" :: String)
+                                H.div $ do
+                                    fromRight . P.runPure . PWH.writeHtml5 P.def $ fromJust (deckCredits deck)
                     displayFooter
 
 displayDeckExercise :: Maybe UserIdentity -> Deck -> H.Html
