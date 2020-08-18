8a41aa6f39b6521bef61b0148996aa82d916ab73
Jonathan Queiroz <dev@johnjq.com>
2020-08-17 21:02:01 -0700

    Add credits for The Crash Course

--- haskell/src/Courses/English/Grammar/Crash/Course.hs
+++ haskell/src/Courses/English/Grammar/Crash/Course.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 -- | This modules exposes the overall "The Crash Course" course.
 module Courses.English.Grammar.Crash.Course (course) where
@@ -6,6 +7,9 @@ module Courses.English.Grammar.Crash.Course (course) where
 import Core
 import Courses.English.Grammar.Crash.Lessons
 import Language.Lojban.Dictionaries (englishDictionary)
+import Courses.Framework.DocumentBuilders (buildDocumentFromMarkdownCode)
+import qualified Text.Pandoc as P
+import Data.FileEmbed (embedStringFile)
 
 -- | Course style.
 style :: CourseStyle
@@ -18,11 +22,14 @@ style = CourseStyle color1 iconUrl where
         -- Source: https://www.flaticon.com/free-icon/jigsaw_993723#term=jigsaw&page=1&position=3
         "https://image.flaticon.com/icons/svg/993/993723.svg"
 
+-- | Course credits.
+credits :: P.Pandoc
+Right credits = buildDocumentFromMarkdownCode $(embedStringFile "resources/courses/english/grammar/crash/credits.md")
+
 -- | Course: The Crash Course.
 course :: Course
-course = Course "crash-course_eng" title shortDescription longDescription credits style englishDictionary lessons where
+course = Course "crash-course_eng" title shortDescription longDescription (Just credits) style englishDictionary lessons where
     title = "The Crash Course (pre-alpha)"
     shortDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
     longDescription = Nothing
-    credits = Nothing
     lessons = [lesson01, lesson02, lesson03]
---
+++ resources/courses/english/grammar/crash/credits.md
@@ -0,0 +1,1 @@
+This course was prepared by [la gleki](gleki.is.my.name@gmail.com), and is also available at [The Crash Course (a draft)](https://mw.lojban.org/papri/The_Crash_Course_(a_draft).
