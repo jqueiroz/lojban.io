f9a8f58a23a1a41d0eacaaed6096b23fef96f18e
Jonathan Queiroz <dev@johnjq.com>
2020-09-03 21:46:48 -0700

    Document functions

--- haskell/src/Language/Lojban/Canonicalization.hs
+++ haskell/src/Language/Lojban/Canonicalization.hs
@@ -10,9 +10,13 @@ import Language.Lojban.Core
 import Language.Lojban.Canonicalization.Internals
 import Util (replaceSubexpression)
 
+-- | Basic general-purpose sentence canonicalizer.
 basicSentenceCanonicalizer :: SentenceCanonicalizer
 basicSentenceCanonicalizer = canonicalizeText
 
+-- | Simple extension of 'basicSentenceCanonicalizer' which disregards (potentially important) semantic variations.
+--
+-- Currently, all it does is replace "da", "de", and "di" with "zo'e", essentially treating all these qualifiers as the same.
 extendedSentenceCanonicalizer :: SentenceCanonicalizer
 extendedSentenceCanonicalizer = basicSentenceCanonicalizer . replaceQuantifiers where
     replaceQuantifiers = (replaceSubexpression "da" "zo'e") . (replaceSubexpression "de" "zo'e") . (replaceSubexpression "di" "zo'e")
