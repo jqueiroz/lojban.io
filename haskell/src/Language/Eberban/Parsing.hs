c6b6aaf20241101ce472f10b4504dfaf93989b67
Jonathan Queiroz <dev@johnjq.com>
2021-11-24 21:45:17 -0800

    Fix typo

--- haskell/src/Language/Eberban/Parsing.hs
+++ haskell/src/Language/Eberban/Parsing.hs
@@ -5,6 +5,6 @@ module Language.Eberban.Parsing
 import qualified Data.Text as T
 import qualified Language.Eberban.Parser.Experimental as ExperimentalParser
 
--- | Parses Lojban text using "Language.Eberban.Parser.Experimental".
+-- | Parses Eberban text using "Language.Eberban.Parser.Experimental".
 parseText :: T.Text -> Either String String
 parseText = ExperimentalParser.parse . T.unpack
