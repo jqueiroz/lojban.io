582e19d07b5e7868e13087965518930a4c7a1edf
Jonathan Queiroz <dev@johnjq.com>
2023-11-28 04:49:09 -0300

    Attempt to set up hls

--- buildscripts/environments/haskell.nix
+++ buildscripts/environments/haskell.nix
@@ -3,8 +3,15 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "haskell-environment";
 
+  nativeBuildInputs = [
+  ];
+
   buildInputs = [
     stack
     haskellPackages.hoogle hlint
+    (haskellPackages.haskell-language-server.overrideAttrs (oldAttrs: {
+      #supportedGhcVersions = [ ];
+      supportedGhcVersions = [ "884" ]; # lts-16.31
+    }))
   ];
 }
---
+++ buildscripts/nvim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+# Change directory to the project's root
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR/.."
+
+export PATH="$DIR/hls":$PATH
+nvim $*
