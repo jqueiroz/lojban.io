cad1ca6dc25f6bb17752f5c9cba9d2542a24a2cc
Jonathan Queiroz <dev@johnjq.com>
2026-02-10 18:25:59 -0300

    Update zoho verification code

--- virtualization/dnscontrol/dnsconfig.js
+++ virtualization/dnscontrol/dnsconfig.js
@@ -12,7 +12,7 @@ D("lojban.io", registrar, DnsProvider(provider),
 
     // Email records
     TXT("zohomail._domainkey", "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDG6dRlC3PgbyzTRCMRS5Z8+WCKlzlWS3R9Ix7Vgc3jK4ECCrZoc5lWQOGMxp69kC11P52P6vMfmy6GELm79cDdzeSwnD7pBjjBsPzLYhTx1hYcwZCGcUdHeERW1jsjJn5QurAxsGL4RmYOSveKLZIQcQEbcQwoq3NEGF5oy2yUXwIDAQAB"),
-    TXT("@", "zoho-verification=zb54519205.zmverify.zoho.com"),
+    TXT("@", "zoho-verification=D2QQVYW1Z7.zmverify.zoho.com"),
     TXT("@", "v=spf1 include:zoho.com ~all"),
 
     MX("@", 10, "mx.zoho.com."),
